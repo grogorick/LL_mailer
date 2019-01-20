85c30da60bb2e8409e7b1780a068fd0635c7f006
Steve Grogorick <steve@grogorick.de>
2019-01-20 16:37:51 +0100

    zip Script

---
+++ zip_plugin.bat
@@ -0,0 +1,9 @@
+"C:\Program Files\7-Zip\7z.exe" a -tzip -spf2 -xr!links.txt LL_mailer__.zip ^
+cssin/src ^
+cssin/vendor ^
+cssin/gpl.txt ^
+cssin/README.md ^
+icon.png ^
+LICENSE ^
+LL_mailer.php ^
+README.md
